d7dd65d215d05f2cdaa41aff176b6c82f722bb31
Austin Heyne <aheyne@ccri.com>
2017-04-24 15:58:07 -0400

    Updating license headers and adding license check
    Signed-off-by: Austin Heyne <aheyne@ccri.com>

--- geomesa-examples-gdelt/src/main/java/com/example/geomesa/gdelt/GDELTQuery.java
+++ geomesa-examples-gdelt/src/main/java/com/example/geomesa/gdelt/GDELTQuery.java
@@ -1,3 +1,11 @@
+/***********************************************************************
+ * Copyright (c) 2014-2017 Commonwealth Computer Research, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution and is available at
+ * http://www.opensource.org/licenses/apache2.0.php.
+ ***********************************************************************/
+
 package com.example.geomesa.gdelt;
 
 import org.apache.commons.cli.BasicParser;
