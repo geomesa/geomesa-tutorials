52486f8110792fe79eed425d75381a2a3178261c
Emilio <elahrvivaz@ccri.com>
2017-11-08 16:34:44 -0500

    Updating GeoMesa version to 1.4.0.0-SNAPSHOT (#53)
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-quickstart-kafka/src/main/java/com/example/geomesa/kafka/KafkaQuickStart.java
+++ geomesa-quickstart-kafka/src/main/java/com/example/geomesa/kafka/KafkaQuickStart.java
@@ -6,9 +6,14 @@
  * http://www.opensource.org/licenses/apache2.0.php.
  ***********************************************************************/
 
-package com.example.geomesa.kafka10;
+package com.example.geomesa.kafka;
 
-import org.apache.commons.cli.*;
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
 import org.geotools.data.DataStore;
 import org.geotools.data.DataStoreFinder;
 import org.geotools.data.FeatureStore;
@@ -17,15 +22,12 @@ import org.geotools.data.simple.SimpleFeatureIterator;
 import org.geotools.data.simple.SimpleFeatureSource;
 import org.geotools.data.simple.SimpleFeatureStore;
 import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.factory.Hints;
 import org.geotools.feature.DefaultFeatureCollection;
 import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
-import org.joda.time.Duration;
 import org.joda.time.Instant;
-import org.locationtech.geomesa.kafka.KafkaDataStoreHelper;
-import org.locationtech.geomesa.kafka.ReplayConfig;
-import org.locationtech.geomesa.kafka.ReplayTimeHelper;
 import org.locationtech.geomesa.utils.geotools.SimpleFeatureTypes;
 import org.locationtech.geomesa.utils.text.WKTUtils$;
 import org.opengis.feature.Property;
@@ -36,49 +38,42 @@ import org.opengis.filter.Filter;
 import org.opengis.filter.FilterFactory2;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Random;
 
 public class KafkaQuickStart {
-    public static final String KAFKA_BROKER_PARAM = "brokers";
-    public static final String ZOOKEEPERS_PARAM = "zookeepers";
-    public static final String ZK_PATH = "zkPath";
-    public static final String VISIBILITY = "visibility";
-
-    public static final String[] KAFKA_CONNECTION_PARAMS = new String[] {
-            KAFKA_BROKER_PARAM,
-            ZOOKEEPERS_PARAM,
-            ZK_PATH
-    };
 
     // reads and parse the command line args
     public static Options getCommonRequiredOptions() {
         Options options = new Options();
 
-        Option kafkaBrokers = OptionBuilder.withArgName(KAFKA_BROKER_PARAM)
+        Option kafkaBrokers = OptionBuilder.withArgName("brokers")
                 .hasArg()
                 .isRequired()
                 .withDescription("The comma-separated list of Kafka brokers, e.g. localhost:9092")
-                .create(KAFKA_BROKER_PARAM);
+                .create("brokers");
         options.addOption(kafkaBrokers);
 
-        Option zookeepers = OptionBuilder.withArgName(ZOOKEEPERS_PARAM)
+        Option zookeepers = OptionBuilder.withArgName("zookeepers")
                 .hasArg()
                 .isRequired()
                 .withDescription("The comma-separated list of Zookeeper nodes that support your Kafka instance, e.g.: zoo1:2181,zoo2:2181,zoo3:2181")
-                .create(ZOOKEEPERS_PARAM);
+                .create("zookeepers");
         options.addOption(zookeepers);
 
-        Option zkPath = OptionBuilder.withArgName(ZK_PATH)
+        Option zkPath = OptionBuilder.withArgName("zkPath")
                 .hasArg()
                 .withDescription("Zookeeper's discoverable path for metadata, defaults to /geomesa/ds/kafka")
-                .create(ZK_PATH);
+                .create("zkPath");
         options.addOption(zkPath);
 
-        Option visibility = OptionBuilder.withArgName(VISIBILITY)
+        Option visibility = OptionBuilder.withArgName("visibility")
                 .hasArg()
-                .create(VISIBILITY);
+                .create("visibility");
         options.addOption(visibility);
-
 
         Option automated = OptionBuilder.withArgName("automated")
                 .create("automated");
@@ -90,9 +85,9 @@ public class KafkaQuickStart {
     // construct connection parameters for the DataStoreFinder
     public static Map<String, String> getKafkaDataStoreConf(CommandLine cmd) {
         Map<String, String> dsConf = new HashMap<>();
-        for (String param : KAFKA_CONNECTION_PARAMS) {
-            dsConf.put(param, cmd.getOptionValue(param));
-        }
+        dsConf.put("kafka.brokers", cmd.getOptionValue("brokers"));
+        dsConf.put("kafka.zookeepers", cmd.getOptionValue("zookeepers"));
+        dsConf.put("kafka.zk.path", cmd.getOptionValue("zkPath"));
         return dsConf;
     }
 
@@ -123,12 +118,14 @@ public class KafkaQuickStart {
             builder.add(MIN_DATE.plusSeconds((int) Math.round(random.nextDouble() * SECONDS_PER_YEAR)).toDate()); // dtg
             builder.add(WKTUtils$.MODULE$.read("POINT(" + (MIN_X + DX * i) + " " + (MIN_Y + DY * i) + ")")); // geom
             SimpleFeature feature1 = builder.buildFeature("1");
+            feature1.getUserData().put(Hints.USE_PROVIDED_FID, Boolean.TRUE);
 
             builder.add(PEOPLE_NAMES[(i+1) % PEOPLE_NAMES.length]); // name
             builder.add((int) Math.round(random.nextDouble()*110)); // age
             builder.add(MIN_DATE.plusSeconds((int) Math.round(random.nextDouble() * SECONDS_PER_YEAR)).toDate()); // dtg
             builder.add(WKTUtils$.MODULE$.read("POINT(" + (MIN_X + DX * i) + " " + (MAX_Y - DY * i) + ")")); // geom
             SimpleFeature feature2 = builder.buildFeature("2");
+            feature2.getUserData().put(Hints.USE_PROVIDED_FID, Boolean.TRUE);
 
             if (visibility != null) {
                 feature1.getUserData().put("geomesa.feature.visibility", visibility);
@@ -188,9 +185,9 @@ public class KafkaQuickStart {
 
         // create the producer and consumer KafkaDataStore objects
         Map<String, String> dsConf = getKafkaDataStoreConf(cmd);
-        dsConf.put("isProducer", "true");
+        dsConf.put("kafka.consumer.count", "0");
         DataStore producerDS = DataStoreFinder.getDataStore(dsConf);
-        dsConf.put("isProducer", "false");
+        dsConf.put("kafka.consumer.count", "1");
         DataStore consumerDS = DataStoreFinder.getDataStore(dsConf);
 
         // verify that we got back our KafkaDataStore objects properly
@@ -201,90 +198,65 @@ public class KafkaQuickStart {
             throw new Exception("Null consumer KafkaDataStore");
         }
 
-        // create the schema which creates a topic in Kafka
-        // (only needs to be done once)
-        final String sftName = "KafkaQuickStart10";
-        final String sftSchema = "name:String,age:Int,dtg:Date,*geom:Point:srid=4326";
-        SimpleFeatureType sft = SimpleFeatureTypes.createType(sftName, sftSchema);
-        // set zkPath to default if not specified
-        String zkPath = (dsConf.get(ZK_PATH) == null) ? "/geomesa/ds/kafka" : dsConf.get(ZK_PATH);
-        SimpleFeatureType preppedOutputSft = KafkaDataStoreHelper.createStreamingSFT(sft, zkPath);
-        // only create the schema if it hasn't been created already
-        if (!Arrays.asList(producerDS.getTypeNames()).contains(sftName))
-            producerDS.createSchema(preppedOutputSft);
-        if (!cmd.hasOption("automated")) {
-            System.out.println("Register KafkaDataStore in GeoServer (Press enter to continue)");
-            System.in.read();
-        }
+        try {
+            // create the schema which creates a topic in Kafka
+            // (only needs to be done once)
+            final String sftName = "KafkaQuickStart";
+            final String sftSchema = "name:String,age:Int,dtg:Date,*geom:Point:srid=4326";
+            SimpleFeatureType sft = SimpleFeatureTypes.createType(sftName, sftSchema);
+            producerDS.createSchema(sft);
 
-        // the live consumer must be created before the producer writes features
-        // in order to read streaming data.
-        // i.e. the live consumer will only read data written after its instantiation
-        SimpleFeatureSource consumerFS = consumerDS.getFeatureSource(sftName);
-        SimpleFeatureStore producerFS = (SimpleFeatureStore) producerDS.getFeatureSource(sftName);
+            if (!cmd.hasOption("automated")) {
+                System.out.println("Register KafkaDataStore in GeoServer (Press enter to continue)");
+                System.in.read();
+            }
 
-        // creates and adds SimpleFeatures to the producer every 1/5th of a second
-        System.out.println("Writing features to Kafka... refresh GeoServer layer preview to see changes");
-        Instant replayStart = new Instant();
+            // the live consumer must be created before the producer writes features
+            // in order to read streaming data.
+            // i.e. the live consumer will only read data written after its instantiation
+            SimpleFeatureSource consumerFS = consumerDS.getFeatureSource(sftName);
+            SimpleFeatureStore producerFS = (SimpleFeatureStore) producerDS.getFeatureSource(sftName);
 
-        String vis = cmd.getOptionValue(VISIBILITY);
-        if(vis != null) System.out.println("Writing features with " + vis);
-        addSimpleFeatures(sft, producerFS, vis);
-        Instant replayEnd = new Instant();
+            // creates and adds SimpleFeatures to the producer every 1/5th of a second
+            System.out.println("Writing features to Kafka... refresh GeoServer layer preview to see changes");
+            Instant replayStart = new Instant();
 
-        // read from Kafka after writing all the features.
-        // LIVE CONSUMER - will obtain the current state of SimpleFeatures
-        System.out.println("\nConsuming with the live consumer...");
-        SimpleFeatureCollection featureCollection = consumerFS.getFeatures();
-        System.out.println(featureCollection.size() + " features were written to Kafka");
+            String vis = cmd.getOptionValue("visibility");
+            if(vis != null) System.out.println("Writing features with " + vis);
+            addSimpleFeatures(sft, producerFS, vis);
+            Instant replayEnd = new Instant();
 
-        addDeleteNewFeature(sft, producerFS);
+            // read from Kafka after writing all the features.
+            // LIVE CONSUMER - will obtain the current state of SimpleFeatures
+            System.out.println("\nConsuming with the live consumer...");
+            SimpleFeatureCollection featureCollection = consumerFS.getFeatures();
+            System.out.println(featureCollection.size() + " features were written to Kafka");
 
-        // read from Kafka after writing all the features.
-        // LIVE CONSUMER - will obtain the current state of SimpleFeatures
-        System.out.println("\nConsuming with the live consumer...");
-        featureCollection = consumerFS.getFeatures();
-        System.out.println(featureCollection.size() + " features were written to Kafka");
+            addDeleteNewFeature(sft, producerFS);
 
-        // the state of the two SimpleFeatures is real time here
-        System.out.println("Here are the two SimpleFeatures that were obtained with the live consumer:");
-        SimpleFeatureIterator featureIterator = featureCollection.features();
-        SimpleFeature feature1 = featureIterator.next();
-        SimpleFeature feature2 = featureIterator.next();
-        featureIterator.close();
-        printFeature(feature1);
-        printFeature(feature2);
+            // read from Kafka after writing all the features.
+            // LIVE CONSUMER - will obtain the current state of SimpleFeatures
+            System.out.println("\nConsuming with the live consumer...");
+            featureCollection = consumerFS.getFeatures();
+            System.out.println(featureCollection.size() + " features were written to Kafka");
 
-        // REPLAY CONSUMER - will obtain the state of SimpleFeatures at any specified time
-        // Replay consumer requires a ReplayConfig which takes a time range and a
-        // duration of time to process
-        System.out.println("\nConsuming with the replay consumer...");
-        Duration readBehind = new Duration(1000); // 1 second readBehind
-        ReplayConfig rc = new ReplayConfig(replayStart, replayEnd, readBehind);
-        SimpleFeatureType replaySFT = KafkaDataStoreHelper.createReplaySFT(preppedOutputSft, rc);
-        producerDS.createSchema(replaySFT);
-        SimpleFeatureSource replayConsumerFS = consumerDS.getFeatureSource(replaySFT.getName());
+            // the state of the two SimpleFeatures is real time here
+            System.out.println("Here are the two SimpleFeatures that were obtained with the live consumer:");
+            SimpleFeatureIterator featureIterator = featureCollection.features();
+            SimpleFeature feature1 = featureIterator.next();
+            SimpleFeature feature2 = featureIterator.next();
+            featureIterator.close();
+            printFeature(feature1);
+            printFeature(feature2);
 
-        // querying for the state of SimpleFeatures approximately 5 seconds before the replayEnd.
-        // the ReplayKafkaConsumerFeatureSource will build the state of SimpleFeatures
-        // by processing all of the messages that were sent in between queryTime-readBehind and queryTime.
-        // only the messages in between replayStart and replayEnd are cached.
-        Instant queryTime = replayEnd.minus(5000);
-        featureCollection = replayConsumerFS.getFeatures(ReplayTimeHelper.toFilter(queryTime));
-        System.out.println(featureCollection.size() + " features were written to Kafka");
-
-        System.out.println("Here are the two SimpleFeatures that were obtained with the replay consumer:");
-        featureIterator = featureCollection.features();
-        feature1 = featureIterator.next();
-        feature2 = featureIterator.next();
-        featureIterator.close();
-        printFeature(feature1);
-        printFeature(feature2);
-
-        if (System.getProperty("clear") != null) {
-            // Run Java command with -Dclear=true
-            // This will cause a 'clear'
-            producerFS.removeFeatures(Filter.INCLUDE);
+            if (System.getProperty("clear") != null) {
+                // Run Java command with -Dclear=true
+                // This will cause a 'clear'
+                producerFS.removeFeatures(Filter.INCLUDE);
+            }
+        } finally {
+            producerDS.dispose();
+            consumerDS.dispose();
         }
 
         System.exit(0);
