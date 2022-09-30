8ae404d690a4ceae65666a4c454fc1cdd451afd4
Austin Heyne <aheyne@ccri.com>
2022-09-30 11:57:51 -0400

    Add GeoMesa Kafka Streams quickstart
    Signed-off-by: Austin Heyne <aheyne@ccri.com>

---
+++ geomesa-tutorials-kafka/geomesa-tutorials-kafka-streams-quickstart/README.md
@@ -0,0 +1,4 @@
+GeoMesa Kafka Streams Quick-Start Tutorial
+==================================
+
+See the official GeoMesa [documentation](http://www.geomesa.org/documentation/tutorials/geomesa-quickstart-kafka-streams.html) for instructions.
---
+++ geomesa-tutorials-kafka/geomesa-tutorials-kafka-streams-quickstart/src/main/java/org/geomesa/example/kafka/GeoPartitioner.java
@@ -0,0 +1,33 @@
+package org.geomesa.example.kafka;
+
+import org.apache.kafka.streams.kstream.KeyValueMapper;
+import org.locationtech.geomesa.curve.Z2SFC;
+import org.locationtech.geomesa.kafka.streams.GeoMesaMessage;
+import org.locationtech.geomesa.utils.geohash.GeohashUtils;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.Point;
+
+class GeoPartitioner implements KeyValueMapper<String, GeoMesaMessage, String> {
+
+  private final Short partitionNumBits;
+  private final Integer defaultGeomIndex;
+  private final Z2SFC z2;
+
+  public GeoPartitioner(Short partitionNumBits, Integer defaultGeomIndex) {
+    this.partitionNumBits = partitionNumBits;
+    this.defaultGeomIndex = defaultGeomIndex;
+    this.z2 = new Z2SFC(partitionNumBits / 2);
+  }
+
+  private String getZBin(Geometry geom) {
+    Point safeGeom = GeohashUtils.getInternationalDateLineSafeGeometry(geom).get().getCentroid();
+    Long index = z2.index(safeGeom.getX(), safeGeom.getY(), false);
+    return String.format("%0" + partitionNumBits + "d", index);
+  }
+
+  @Override
+  public String apply(String key, GeoMesaMessage value) {
+    Geometry geom = (Geometry) value.attributes().apply(defaultGeomIndex);
+    return getZBin(geom);
+  }
+}
